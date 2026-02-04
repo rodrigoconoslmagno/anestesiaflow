6b53f3cd1fcebd46cf3188aec02eb72d2f4c0d88
Rodrigo Consolmagno <rodrigoconsolmagno@hotmail.com>
2026-02-03 21:41:20 -0300

    Ajuste no cors

--- backend/src/main/java/br/com/anestesiaflow/auth/security/SecurityConfig.java
+++ backend/src/main/java/br/com/anestesiaflow/auth/security/SecurityConfig.java
@@ -63,7 +63,8 @@ public class SecurityConfig {
 		configuration.setAllowedOrigins(Arrays.asList(
 		        "http://localhost:5173", 
 		        "http://127.0.0.1:5173",
-		        "http://192.168.0.237:5173" // IP fixo do seu Mac
+		        "http://192.168.0.237:5173", // IP fixo do seu Mac
+		        "https://anestesiaflow.onrender.com"
 		    ));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));

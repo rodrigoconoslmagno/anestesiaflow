d1ed0a256ba9a6f1ee61590ec8346fc6c6379abb
Rodrigo Consolmagno <rodrigoconsolmagno@hotmail.com>
2026-02-05 12:08:12 -0300

    Erro de deploy

--- frontend/src/paginas/Usuario.tsx
+++ frontend/src/paginas/Usuario.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';

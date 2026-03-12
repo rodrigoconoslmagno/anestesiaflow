c7a9a3d2cbf6ce1ac424793d7af41a87b09dd4c4
Rodrigo Consolmagno <rodrigoconsolmagno@hotmail.com>
2026-03-12 15:11:28 -0300

    Ajuste para nao ter a configuracao no arquivo de propriedades

--- backend/src/main/java/br/com/anestesiaflow/notification/config/FirebaseConfig.java
+++ backend/src/main/java/br/com/anestesiaflow/notification/config/FirebaseConfig.java
@@ -1,14 +1,11 @@
 package br.com.anestesiaflow.notification.config;
 
 import java.io.InputStream;
-
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import jakarta.annotation.PostConstruct;
@@ -16,7 +13,7 @@ import jakarta.annotation.PostConstruct;
 @Configuration
 public class FirebaseConfig {
 	
-	@Value("${app.firebase.config-path}")
+	@Value("${app.firebase.config-path:#{null}}")
     private String configPath;
 
     private final ResourceLoader resourceLoader;
@@ -28,22 +25,21 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() {
         try {
-            
-//        	if (configPath != null && !configPath.isEmpty()) {        	
-//	            Resource resource = resourceLoader.getResource(configPath);
-//	            InputStream serviceAccount = resource.getInputStream();
-//	
-//	            FirebaseOptions options = FirebaseOptions.builder()
-//	                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//	                    .build();
-//	
-//	            if (FirebaseApp.getApps().isEmpty()) {
-//	                FirebaseApp.initializeApp(options);
-//	                System.out.print("Firebase inicializado com sucesso!");
-//	            }
-//        	}
+        	if (configPath != null && !configPath.isEmpty()) {        	
+	            Resource resource = resourceLoader.getResource(configPath);
+	            InputStream serviceAccount = resource.getInputStream();
+	
+	            FirebaseOptions options = FirebaseOptions.builder()
+	                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+	                    .build();
+	
+	            if (FirebaseApp.getApps().isEmpty()) {
+	                FirebaseApp.initializeApp(options);
+	                System.out.print("Firebase inicializado com sucesso!");
+	            }
+        	}
         } catch (Exception e) {
-            System.out.println("FALHA CRÍTICA: Não foi possível inicializar o Firebase. As notificações não funcionarão. Erro: {}" + e.getMessage());
+            System.err.println("FALHA CRÍTICA: Não foi possível inicializar o Firebase. As notificações não funcionarão. Erro: {}" + e.getMessage());
         }
     }
 }
